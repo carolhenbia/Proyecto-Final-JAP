c3fc374077650b8ef0294cc576ef765b40b33cb3
carolhenbia <carolhenbia@outlook.com>
2020-08-12 20:21:00 -0300

    cambiar lugar el set cookie

--- js/login.js
+++ js/login.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         const username = urlParams.get('nombreUsuario');//al objeto le pedis el usuario
         const password = urlParams.get('password')
         if (username == "admin" && password == "1234") {
+            setCookie("isLoggedIn", "1") //da el nombre y valor de la cookie a la funcion
             window.location.href="index.html" //el navegador te manda a otra url 
             //se le saca la barrita para que no sea absoluta sino relativa 
-            setCookie("isLoggedIn", "1") //da el nombre y valor de la cookie a la funcion
         } else {
             alert("Credenciales incorrectas"); 
         }    
