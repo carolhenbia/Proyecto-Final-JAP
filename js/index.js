6793739ec13aa3bd1d15fb725a67000b42778839
carolhenbia <carolhenbia@outlook.com>
2020-08-13 19:42:23 -0300

    probar login denuevo

--- js/index.js
+++ js/index.js
@@ -9,12 +9,11 @@ var getCookie = function (name) { //funcion pre hecha que parsea los datos de co
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + name + "=");
 	if (parts.length == 2) return parts.pop().split(";").shift();
-};
- */
-/* document.addEventListener("DOMContentLoaded", function(e){
+}; */
+
+document.addEventListener("DOMContentLoaded", function(e){
     var isLoggedIn = window.localStorage.getItem("isLoggedIn") //pide la cookie 
     if(isLoggedIn == undefined){ //si ve que el loggedin es igual a undefined (osea sin cookie)
         window.location.href = "login.html" //lo manda al login 
     }
 });
- */
--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-/* document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e){
     const queryString = window.location.search; //me da lo que esta del signo de pregunta en adelante
     if(queryString !== "") {
         const urlParams = new URLSearchParams(queryString); //lo parsea a objeto 
         const username = urlParams.get('nombreUsuario');//al objeto le pedis el usuario
         const password = urlParams.get('password')
-        if (username == "admin" && password == "1234") {
-            setCookie("isLoggedIn", "1") //da el nombre y valor de la cookie a la funcion
+        if (username != "" && password != "") {
+            window.localStorage.setItem("isLoggedIn", username) //setItem es una funcion publica recibe parametro valor y guarda ahi
             window.location.href="index.html" //el navegador te manda a otra url 
             //se le saca la barrita para que no sea absoluta sino relativa 
         } else {
@@ -17,13 +17,15 @@
     }
 });
 
-var setCookie = function(name, value) {
+/*var setCookie = function(name, value) {
     document.cookie = name+'='+value+'; expires=Fri, 31 Dec 2020 23:59:59 GMT'; } */
-/* //Coloca la cookie y pone la fecha en que expira 
+//Coloca la cookie y pone la fecha en que expira 
 
 //window es una variable del navegador que te da herramientas para manejarlo
 
-/* function onSignIn(googleUser) {
+function onSignIn(googleUser) {
+    console.log(googleUser)
+    window.localStorage.setItem("isLoggedIn", googleUser) //setItem es una funcion publica recibe parametro valor y guarda ahi
     window.location.href = "index.html";
-} */
+}
 
