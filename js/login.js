1a1c2893467972d402a8d2b139530551d59f3d53
carolhenbia <carolhenbia@outlook.com>
2020-08-13 19:57:35 -0300

    probar sign out

--- js/login.js
+++ js/login.js
@@ -2,6 +2,10 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () { //el then hace que despues de terminar lo anterior, hace lo siguiente
+        console.log('User signed out.');
+      });
     const queryString = window.location.search; //me da lo que esta del signo de pregunta en adelante
     if(queryString !== "") {
         const urlParams = new URLSearchParams(queryString); //lo parsea a objeto 
@@ -16,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         }    
     }
 });
+
 
 /*var setCookie = function(name, value) {
     document.cookie = name+'='+value+'; expires=Fri, 31 Dec 2020 23:59:59 GMT'; } */
