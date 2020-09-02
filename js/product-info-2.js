be88395e8890caf0275c9a4141efb5aab8cf844b
carolhenbia <carolhenbia@outlook.com>
2020-09-02 19:45:50 -0300

    formulario enviar comentario

--- js/product-info-2.js
+++ js/product-info-2.js
@@ -31,10 +31,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
             document.getElementById("imagen3Producto").src = productImages[2];
             document.getElementById("imagen4Producto").src = productImages[3];
             document.getElementById("imagen5Producto").src = productImages[4];
-            document.getElementById("tituloComentarios").innerHTML = `Opiniones del auto ${productName}`;
+            /* document.getElementById("tituloComentarios").innerHTML = `Opiniones del auto ${productName}`; */
         }
     });
 });
+
+
+
+
+
+/* var emailComentario = document.getElementById("nombreEmail"); */
+/* var fechaActual = new Date();
+document.write(fechaActual.getDate() + "/" + (fechaActual.getMonth() +1) + "/" + fechaActual.getFullYear()); */
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (result) {
@@ -42,11 +51,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
             productComments = result.data;
             console.log(result.data)
             let htmlContentToAppend = ""
-            
+
             for (let i = 0; i < productComments.length; i++) {
                 let comentario = productComments[i]; //revisa el array de resultados, producto por producto
                 console.log(comentario.user)
-                
+
                 htmlContentToAppend += `
                         <div class="media mt-3 mb-4">
                   <img class="d-flex mr-3 z-depth-1" src="/img/thumbComentario.png" width="62">
@@ -69,77 +78,71 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 </div>
                         `
                 document.getElementById("reviews").innerHTML = htmlContentToAppend;
-            } 
-    }
-});
-});
+            }
 
-
-
-/* 
-var objetoPrueba = [{
-    "score": 3,
-    "description": "Ya llevo un año con este auto y la verdad que tiene sus ventajas y desventajas",
-    "user": "juan_pedro",
-    "dateTime": "2020-02-25 18:03:52"
-},
-{
-    "score": 5,
-    "description": "Es un auto muy cómodo y en relación precio/calidad vale la pena!",
-    "user": "maria_sanchez",
-    "dateTime": "2020-01-17 13:42:18"
-},
-{
-    "score": 4,
-    "description": "Casi todo bien!, excepto por algún detalle de gusto personal",
-    "user": "paola_perez",
-    "dateTime": "2020-03-14 09:05:13"
-},
-{
-    "score": 5,
-    "description": "Un espectáculo el auto!",
-    "user": "pablo_gomez",
-    "dateTime": "2020-02-21 15:05:22"
-}]
-
-let htmlContentToAppend = "";
-for (let i = 0; i < objetoPrueba.length; i++) {
-    let comentario = objetoPrueba[i]; //revisa el array de resultados, producto por producto
-    console.log(comentario.user)
-    
-    htmlContentToAppend += `
-            <div class="media mt-3 mb-4">
-      <img class="d-flex mr-3 z-depth-1" src="/img/thumbComentario.png" width="62">
-      <div class="media-body">
-        <div class="d-sm-flex justify-content-between">
-          <p class="mt-1 mb-2">
-            <strong> ${comentario.user} </strong>
-            <span>– </span><span>${comentario.dateTime}</span>
-          </p>
-          <ul class="rating mb-sm-0">
-              <i class="fas fa-star fa-sm text-primary"></i>
-              <i class="fas fa-star fa-sm text-primary"></i>
-              <i class="fas fa-star fa-sm text-primary"></i>
-              <i class="far fa-star fa-sm text-primary"></i>
-              <i class="far fa-star fa-sm text-primary"></i>
-          </ul>
+            let formularioDeComentario = ""
+            formularioDeComentario = `
+            
+              <h5 class="mt-4">Agrega tu opinión</h5>
+              <div class="my-3">
+                      <i class="fas fa-star fa-sm text-primary"></i>
+                      <i class="fas fa-star fa-sm text-primary"></i>
+                      <i class="fas fa-star fa-sm text-primary"></i>
+                      <i class="fas fa-star fa-sm text-primary"></i>
+                      <i class="far fa-star fa-sm text-primary"></i>
+              </div>
+              <div>
+                <!-- Your review -->
+                <div class="md-form md-outline">
+                  <textarea id="comentarioNuevo" class="md-textarea form-control pr-6" rows="4" placeholder="Tu opinión o comentario" maxlength="200"></textarea>
+                  <label for="form76"></label>
+                </div>
+                <!-- Name -->
+                <div class="md-form md-outline">
+                  <input type="text" id="comentarioNombre" class="form-control pr-6" placeholder="Nombre" maxlength="50">
+                  <label for="form75"></label>
+                </div>
+                <!-- Email -->
+                <div class="md-form md-outline">
+                  <input type="email" id="comentarioEmail" class="form-control pr-6" placeholder="Email">
+                  <label for="form77"></label>
+                </div>
+                <div class="text-right pb-2">
+                  <button type="button" onclick="enviarComentario();" class="btn btn-primary" style="background-color: #e72a79; border-color: #e72a79;">Comentar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+            </div>
         </div>
-        <p class="mb-0">${comentario.description}</p>
-      </div>
-    </div>
             `
-    document.getElementById("PRUEBA").innerHTML = htmlContentToAppend;
-} 
+            document.getElementById("info").innerHTML = formularioDeComentario;
 
- */ 
-
+            function enviarComentario() {
+                comentario = crearObjetoComentario()
+                productComments.push(comentario);
+            }
+            
+            function crearObjetoComentario() {
+                var comentarioNuevo = document.getElementById("comentarioNuevo");
+                var nombreComentario = document.getElementById("comentarioNombre");
+                var objComentarioNuevo = {
+                    score: null,
+                    description: comentarioNuevo.value,
+                    user: nombreComentario.value,
+                    dateTime: null
+                }
+                return objComentarioNuevo; 
+            } 
+        }
+        
+    });
+});
 
 
 /* function cambiarFoto(){
     foto1.innerHTML = foto2;
 }
-
-
 
 
 /* document.addEventListener("DOMContentLoaded", function (e) {
