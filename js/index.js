daea99de3b6bd2770f72ec51272715244e65e83e
carolhenbia <carolhenbia@outlook.com>
2020-09-15 19:36:49 -0300

    dropdown con iconos en todas las páginas

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     var isLoggedIn = window.localStorage.getItem("isLoggedIn") //agarra el item
     if (isLoggedIn == undefined) { //si ve que el loggedin es igual a undefined 
         window.location.href = "login.html" //lo manda al login 
-    } else {document.getElementById("usuario").innerHTML = `Usuario: ${isLoggedIn}`;}
+    } else {document.getElementById("usuario").innerHTML = `Usuarie: ${isLoggedIn}`;}
 });
 
 /* function onLoad() {
