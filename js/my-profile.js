2486b63715fccb5658cf6c39744a2ad699effdb7
carolhenbia <carolhenbia@outlook.com>
2020-10-13 21:57:01 -0300

    my profile guardado de datos

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,3 +5,49 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 });
 
+
+var datosPersonales = {};
+
+function agregarDatosPersonales() {
+    alert("hola")
+    var nombresUsuario = document.getElementById("nombres");
+    var apellidosUsuario = document.getElementById("apellidos");
+    var edad = document.getElementById("fechaNacimiento");
+    var telefonoUsuario = document.getElementById("telefono");
+    var emailUsuario = document.getElementById("email");
+
+    if ( nombresUsuario.value != "" && apellidosUsuario.value != "" 
+    && edad.value != "" && telefonoUsuario.value != "" && emailUsuario.value != "" ) {
+        var datosPersonales = {
+            nombres: nombresUsuario.value,
+            apellidos: apellidosUsuario.value,
+            edad: edad.value,
+            telefono: telefonoUsuario.value,
+            email: emailUsuario.value
+        };
+        localStorage.setItem("datosPersonales", JSON.stringify(datosPersonales)); 
+        console.log(datosPersonales)
+    }
+}
+
+var contenidoMiPerfil = {};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    contenidoMiPerfil = JSON.parse(localStorage.getItem("datosPersonales"));   
+    console.log(contenidoMiPerfil);
+});
+
+document.addEventListener("DOMContentLoaded", function mostrarMiPerfil(contenidoMiPerfil) {
+    let miPerfil = "";
+    if (contenidoMiPerfil != undefined){
+        miPerfil = `<h1>${contenidoMiPerfil['nombres']}</h1> 
+        `
+    document.getElementById("conPerfil").innerHTML = miPerfil; 
+    console.log(contenidoMiPerfil);   
+    }
+});
+
+
+
+
+
